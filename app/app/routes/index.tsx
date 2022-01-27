e1c62a49d52bf8e8a503f16fd9552c8ac62a995e
Kamil Zagrabski <kamil.zagrabski@gmail.com>
2022-01-27 21:05:35 +0100

    Redirect to the search when index is loaded

--- app/app/routes/index.tsx
+++ app/app/routes/index.tsx
@@ -1,3 +1,9 @@
+import { redirect } from "remix";
+
+export const loader = () => {
+  return redirect("/search");
+};
+
 export default function () {
   return (
     <a href="/search" title="Search for a book">
