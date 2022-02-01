5b2db23bbc44f17a68fc75cc691627ec94223d48
Kamil Zagrabski <kamil.zagrabski@gmail.com>
2022-02-01 20:30:03 +0100

    Style the book page a bit

--- app/app/routes/book/$bookSlug.tsx
+++ app/app/routes/book/$bookSlug.tsx
@@ -1,5 +1,10 @@
 import { useLoaderData } from "remix";
 import books from "../../data/books.json";
+import styles from "~/book/book.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 interface Book {
   title: string;
@@ -22,5 +27,38 @@ export const loader = ({
 export default function Book() {
   const book = useLoaderData<Book | null>();
 
-  return <pre>{JSON.stringify(book, null, 2)}</pre>;
+  return (
+    <article className="book">
+      <nav className="breadcrumbs">
+        <button
+          type="button"
+          onClick={() => history.back()}
+          className="go-back"
+        >
+          &laquo; Go to search
+        </button>
+      </nav>
+
+      <header className="head">
+        <h1 className="title">{book?.title}</h1>
+        <h2 className="authors">Author(s): {book?.authors}</h2>
+        <h3 className="rating">Our rating: {book?.rating} / 5</h3>
+      </header>
+
+      <img
+        src={book?.imageUrl}
+        alt={`Cover of ${book?.title}`}
+        className="image"
+      />
+      
+      <p className="description">{book?.description}</p>
+
+      <div className="offer">
+        <p className="price">
+          {book?.price ? "$" + (book?.price / 100).toFixed(2) : "unknown"}
+        </p>
+        <button>Add to cart</button>
+      </div>
+    </article>
+  );
 }
